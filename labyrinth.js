29de6f6bfd7c737e0e044802bf3bac09f6f00021
densta <pavel.pysarchuk@gmail.com>
2019-05-23 11:13:16 +0300

    Create labyrinth.js

---
+++ labyrinth.js
@@ -0,0 +1,338 @@
+const defaultMap = '[[{"h":0,"w":0,"type":"block"},{"h":0,"w":1,"type":"block"},{"h":0,"w":2,"type":"block"},{"h":0,"w":3,"type":"block"},{"h":0,"w":4,"type":"block"},{"h":0,"w":5,"type":"block"},{"h":0,"w":6,"type":"block"},{"h":0,"w":7,"type":"block"},{"h":0,"w":8,"type":"block"},{"h":0,"w":9,"type":"block"},{"h":0,"w":10,"type":"block"},{"h":0,"w":11,"type":"block"},{"h":0,"w":12,"type":"block"},{"h":0,"w":13,"type":"block"},{"h":0,"w":14,"type":"block"}],[{"h":1,"w":0,"type":"block"},{"h":1,"w":1,"type":"path"},{"h":1,"w":2,"type":"path"},{"h":1,"w":3,"type":"block"},{"h":1,"w":4,"type":"path"},{"h":1,"w":5,"type":"block"},{"h":1,"w":6,"type":"block"},{"h":1,"w":7,"type":"path"},{"h":1,"w":8,"type":"path"},{"h":1,"w":9,"type":"path"},{"h":1,"w":10,"type":"block"},{"h":1,"w":11,"type":"path"},{"h":1,"w":12,"type":"path"},{"h":1,"w":13,"type":"path"},{"h":1,"w":14,"type":"block"}],[{"h":2,"w":0,"type":"block"},{"h":2,"w":1,"type":"path"},{"h":2,"w":2,"type":"block"},{"h":2,"w":3,"type":"block"},{"h":2,"w":4,"type":"path"},{"h":2,"w":5,"type":"block"},{"h":2,"w":6,"type":"block"},{"h":2,"w":7,"type":"path"},{"h":2,"w":8,"type":"block"},{"h":2,"w":9,"type":"path"},{"h":2,"w":10,"type":"block"},{"h":2,"w":11,"type":"path"},{"h":2,"w":12,"type":"block"},{"h":2,"w":13,"type":"path"},{"h":2,"w":14,"type":"block"}],[{"h":3,"w":0,"type":"block"},{"h":3,"w":1,"type":"path"},{"h":3,"w":2,"type":"block"},{"h":3,"w":3,"type":"block"},{"h":3,"w":4,"type":"path"},{"h":3,"w":5,"type":"block"},{"h":3,"w":6,"type":"block"},{"h":3,"w":7,"type":"path"},{"h":3,"w":8,"type":"block"},{"h":3,"w":9,"type":"path"},{"h":3,"w":10,"type":"block"},{"h":3,"w":11,"type":"path"},{"h":3,"w":12,"type":"block"},{"h":3,"w":13,"type":"path"},{"h":3,"w":14,"type":"block"}],[{"h":4,"w":0,"type":"block"},{"h":4,"w":1,"type":"path"},{"h":4,"w":2,"type":"block"},{"h":4,"w":3,"type":"block"},{"h":4,"w":4,"type":"path"},{"h":4,"w":5,"type":"block"},{"h":4,"w":6,"type":"block"},{"h":4,"w":7,"type":"path"},{"h":4,"w":8,"type":"block"},{"h":4,"w":9,"type":"path"},{"h":4,"w":10,"type":"block"},{"h":4,"w":11,"type":"path"},{"h":4,"w":12,"type":"block"},{"h":4,"w":13,"type":"path"},{"h":4,"w":14,"type":"block"}],[{"h":5,"w":0,"type":"block"},{"h":5,"w":1,"type":"path"},{"h":5,"w":2,"type":"block"},{"h":5,"w":3,"type":"block"},{"h":5,"w":4,"type":"path"},{"h":5,"w":5,"type":"block"},{"h":5,"w":6,"type":"block"},{"h":5,"w":7,"type":"path"},{"h":5,"w":8,"type":"block"},{"h":5,"w":9,"type":"path"},{"h":5,"w":10,"type":"block"},{"h":5,"w":11,"type":"path"},{"h":5,"w":12,"type":"block"},{"h":5,"w":13,"type":"path"},{"h":5,"w":14,"type":"block"}],[{"h":6,"w":0,"type":"block"},{"h":6,"w":1,"type":"path"},{"h":6,"w":2,"type":"block"},{"h":6,"w":3,"type":"path"},{"h":6,"w":4,"type":"path"},{"h":6,"w":5,"type":"path"},{"h":6,"w":6,"type":"block"},{"h":6,"w":7,"type":"path"},{"h":6,"w":8,"type":"block"},{"h":6,"w":9,"type":"path"},{"h":6,"w":10,"type":"block"},{"h":6,"w":11,"type":"path"},{"h":6,"w":12,"type":"block"},{"h":6,"w":13,"type":"path"},{"h":6,"w":14,"type":"block"}],[{"h":7,"w":0,"type":"block"},{"h":7,"w":1,"type":"path"},{"h":7,"w":2,"type":"path"},{"h":7,"w":3,"type":"path"},{"h":7,"w":4,"type":"path"},{"h":7,"w":5,"type":"path"},{"h":7,"w":6,"type":"path"},{"h":7,"w":7,"type":"path"},{"h":7,"w":8,"type":"path"},{"h":7,"w":9,"type":"path"},{"h":7,"w":10,"type":"path"},{"h":7,"w":11,"type":"path"},{"h":7,"w":12,"type":"block"},{"h":7,"w":13,"type":"path"},{"h":7,"w":14,"type":"block"}],[{"h":8,"w":0,"type":"block"},{"h":8,"w":1,"type":"path"},{"h":8,"w":2,"type":"block"},{"h":8,"w":3,"type":"path"},{"h":8,"w":4,"type":"path"},{"h":8,"w":5,"type":"path"},{"h":8,"w":6,"type":"block"},{"h":8,"w":7,"type":"path"},{"h":8,"w":8,"type":"block"},{"h":8,"w":9,"type":"path"},{"h":8,"w":10,"type":"block"},{"h":8,"w":11,"type":"block"},{"h":8,"w":12,"type":"block"},{"h":8,"w":13,"type":"path"},{"h":8,"w":14,"type":"block"}],[{"h":9,"w":0,"type":"block"},{"h":9,"w":1,"type":"path"},{"h":9,"w":2,"type":"block"},{"h":9,"w":3,"type":"block"},{"h":9,"w":4,"type":"path"},{"h":9,"w":5,"type":"block"},{"h":9,"w":6,"type":"block"},{"h":9,"w":7,"type":"path"},{"h":9,"w":8,"type":"block"},{"h":9,"w":9,"type":"path"},{"h":9,"w":10,"type":"block"},{"h":9,"w":11,"type":"path"},{"h":9,"w":12,"type":"path"},{"h":9,"w":13,"type":"path"},{"h":9,"w":14,"type":"block"}],[{"h":10,"w":0,"type":"block"},{"h":10,"w":1,"type":"path"},{"h":10,"w":2,"type":"block"},{"h":10,"w":3,"type":"block"},{"h":10,"w":4,"type":"path"},{"h":10,"w":5,"type":"block"},{"h":10,"w":6,"type":"block"},{"h":10,"w":7,"type":"path"},{"h":10,"w":8,"type":"block"},{"h":10,"w":9,"type":"path"},{"h":10,"w":10,"type":"block"},{"h":10,"w":11,"type":"path"},{"h":10,"w":12,"type":"block"},{"h":10,"w":13,"type":"block"},{"h":10,"w":14,"type":"block"}],[{"h":11,"w":0,"type":"block"},{"h":11,"w":1,"type":"path"},{"h":11,"w":2,"type":"block"},{"h":11,"w":3,"type":"block"},{"h":11,"w":4,"type":"path"},{"h":11,"w":5,"type":"block"},{"h":11,"w":6,"type":"block"},{"h":11,"w":7,"type":"path"},{"h":11,"w":8,"type":"block"},{"h":11,"w":9,"type":"path"},{"h":11,"w":10,"type":"block"},{"h":11,"w":11,"type":"path"},{"h":11,"w":12,"type":"block"},{"h":11,"w":13,"type":"path"},{"h":11,"w":14,"type":"exit"}],[{"h":12,"w":0,"type":"block"},{"h":12,"w":1,"type":"path"},{"h":12,"w":2,"type":"block"},{"h":12,"w":3,"type":"block"},{"h":12,"w":4,"type":"path"},{"h":12,"w":5,"type":"block"},{"h":12,"w":6,"type":"block"},{"h":12,"w":7,"type":"path"},{"h":12,"w":8,"type":"block"},{"h":12,"w":9,"type":"path"},{"h":12,"w":10,"type":"block"},{"h":12,"w":11,"type":"path"},{"h":12,"w":12,"type":"block"},{"h":12,"w":13,"type":"path"},{"h":12,"w":14,"type":"block"}],[{"h":13,"w":0,"type":"block"},{"h":13,"w":1,"type":"path"},{"h":13,"w":2,"type":"path"},{"h":13,"w":3,"type":"block"},{"h":13,"w":4,"type":"path"},{"h":13,"w":5,"type":"block"},{"h":13,"w":6,"type":"block"},{"h":13,"w":7,"type":"path"},{"h":13,"w":8,"type":"path"},{"h":13,"w":9,"type":"path"},{"h":13,"w":10,"type":"block"},{"h":13,"w":11,"type":"path"},{"h":13,"w":12,"type":"path"},{"h":13,"w":13,"type":"path"},{"h":13,"w":14,"type":"block"}],[{"h":14,"w":0,"type":"block"},{"h":14,"w":1,"type":"block"},{"h":14,"w":2,"type":"block"},{"h":14,"w":3,"type":"block"},{"h":14,"w":4,"type":"block"},{"h":14,"w":5,"type":"block"},{"h":14,"w":6,"type":"block"},{"h":14,"w":7,"type":"block"},{"h":14,"w":8,"type":"block"},{"h":14,"w":9,"type":"block"},{"h":14,"w":10,"type":"block"},{"h":14,"w":11,"type":"block"},{"h":14,"w":12,"type":"block"},{"h":14,"w":13,"type":"block"},{"h":14,"w":14,"type":"block"}]]';
+const defaultCharacterPosition = '{"h":7,"w":4}';
+
+class Labyrinth {
+  constructor() {
+    this.mapEditMode = false;
+    this.characterSetMode = false;
+    this.characterPosition = {};
+    this.labyrinthMapArray = [];
+
+    this.loadMap();
+    this.loadCharacterPosition();
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyboard = this.handleKeyboard.bind(this);
+
+    window.moveCharacter = this.moveCharacter.bind(this);
+    window.document.addEventListener('keydown', this.handleKeyboard);
+    window.document.addEventListener('click', this.handleClick);
+
+    this.render();
+  }
+
+  generateNewMap() {
+    const height = 15, width = 15;
+    this.labyrinthMapArray = [];
+    for (let h = 0; h < height; h++) {
+      const widthRow = [];
+      for (let w = 0; w < width; w++) {
+        widthRow[w] = { h, w, type: 'block' };
+      }
+      this.labyrinthMapArray[h] = widthRow;
+    }
+  }
+
+  loadMap() {
+    const labyrinthJSON = window.localStorage.getItem('labyrinth-map') || defaultMap;
+    try {
+      this.labyrinthMapArray = window.JSON.parse(labyrinthJSON);
+    } catch (e) {
+      console.error('Map is corrupted. Creating a new one');
+      this.generateNewMap();
+    }
+  }
+
+  loadCharacterPosition() {
+    const positionJSON = window.localStorage.getItem('character-position') || defaultCharacterPosition;
+    try {
+      const position = window.JSON.parse(positionJSON);
+      const positionCell = this.labyrinthMapArray[position.h][position.w];
+      if (positionCell.type === 'block') {
+        throw new Error('character could not ctand in block');
+      }
+      this.characterPosition = position;
+    } catch (e) {
+      console.error('Character position is corrupted. Reseting', e);
+      window.localStorage.removeItem('character-position');
+      this.characterPosition = { h: -1, w: -1 };
+    }
+  }
+
+  saveCharacterPosition() {
+    window.localStorage.setItem('character-position', window.JSON.stringify(this.characterPosition));
+  }
+
+  handleClick(event) {
+    const positionString = event.target.getAttribute('cellPosition');
+    if (positionString) {
+      const [h, w] = positionString.split(':').map(i => Number.parseInt(i));
+      const cell = this.labyrinthMapArray[h][w]; // intended mutation
+      if (this.characterSetMode) {
+        if (cell.type === 'path') {
+          this.characterPosition = { h, w };
+          this.saveCharacterPosition();
+          this.characterSetMode = false;
+          this.render();
+        } else {
+          console.warn('Character can be placed only on path block');
+        }
+      } else if (this.mapEditMode) {
+        switch (cell.type) {
+          case 'block':
+            cell.type = 'path';
+            break;
+          case 'path':
+            cell.type = 'exit';
+            break;
+          case 'exit':
+            cell.type = 'block';
+            break;
+          default:
+            break;
+        }
+        this.render();
+      }
+    }
+  }
+
+  handleKeyboard(event) {
+    switch (event.key) {
+      case 'ArrowUp':
+      case 'w':
+        this.moveCharacter('up');
+        break;
+      case 'ArrowRight':
+      case 'd':
+        this.moveCharacter('right');
+        break;
+      case 'ArrowDown':
+      case 's':
+        this.moveCharacter('down');
+        break;
+      case 'ArrowLeft':
+      case 'a':
+        this.moveCharacter('left');
+        break;
+      default:
+        break;
+    }
+  }
+
+  moveCharacter(direction) {
+    let targetCell;
+    const { h, w } = this.characterPosition;
+    try {
+      switch (direction) {
+        case 'up':
+          targetCell = this.labyrinthMapArray[h - 1][w];
+          break;
+        case 'right':
+          targetCell = this.labyrinthMapArray[h][w + 1];
+          break;
+        case 'down':
+          targetCell = this.labyrinthMapArray[h + 1][w];
+          break;
+        case 'left':
+          targetCell = this.labyrinthMapArray[h][w - 1];
+          break;
+        default:
+          throw new Error('unexpected direction');
+      }
+    } catch (e) {
+      return 0;
+    }
+
+    if (targetCell.type === 'block') {
+      return 0;
+    }
+
+    this.characterPosition.h = targetCell.h;
+    this.characterPosition.w = targetCell.w;
+    this.saveCharacterPosition();
+    this.render();
+    if (targetCell.type === 'exit') {
+      alert('You reached exit!');
+      return 2;
+    } else {
+      return 1;
+    }
+  }
+
+  getCellElement(cell) {
+    const cellEl = window.document.createElement('span');
+    cellEl.style.width = '30px';
+    cellEl.style.display = 'inline-block';
+    cellEl.style.verticalAlign = 'top';
+    cellEl.style.textAlign = 'center';
+    cellEl.style.fontSize = '22px';
+    cellEl.style.height = '30px';
+    cellEl.style.borderRadius = '4px';
+    cellEl.style.margin = '1px';
+    switch (cell.type) {
+      case 'block':
+        cellEl.style.backgroundColor = 'saddlebrown';
+        break;
+      case 'path':
+        cellEl.style.backgroundColor = 'rgb(210, 210, 210)';
+        break;
+      case 'exit':
+        cellEl.style.backgroundColor = 'rgb(210, 210, 210)';
+        cellEl.innerHTML = '🚪';
+        break;
+      default:
+        break;
+    }
+    const cellPositionString = `${cell.h}:${cell.w}`;
+    const characterPositionString = `${this.characterPosition.h}:${this.characterPosition.w}`;
+
+    if (cellPositionString === characterPositionString) {
+      cellEl.innerHTML = '🚶';
+    }
+
+    cellEl.setAttribute('cellPosition', cellPositionString);
+    return cellEl;
+  }
+
+  getControlPanelElement() {
+    const controlPanelElement = window.document.createElement('div');
+    controlPanelElement.style.padding = '10px 0';
+
+    if (this.mapEditMode) {
+      const saveEditBtn = window.document.createElement('button');
+      saveEditBtn.innerHTML = 'save';
+      saveEditBtn.addEventListener('click', () => {
+        window.localStorage.setItem('labyrinth-map', window.JSON.stringify(this.labyrinthMapArray));
+        this.loadCharacterPosition();
+        this.mapEditMode = false;
+        this.render();
+      });
+      controlPanelElement.appendChild(saveEditBtn);
+
+      const cancelEditBtn = window.document.createElement('button');
+      cancelEditBtn.innerHTML = 'cancel';
+      cancelEditBtn.addEventListener('click', () => {
+        this.mapEditMode = false;
+        this.loadMap();
+        this.render();
+      });
+      controlPanelElement.appendChild(cancelEditBtn);
+
+      const resetMapBtn = window.document.createElement('button');
+      resetMapBtn.innerHTML = 'delete';
+      resetMapBtn.addEventListener('click', () => {
+        this.generateNewMap();
+        this.render();
+      });
+      controlPanelElement.appendChild(resetMapBtn);
+
+      const defaultBtn = window.document.createElement('button');
+      defaultBtn.innerHTML = 'default map';
+      defaultBtn.addEventListener('click', () => {
+        window.localStorage.setItem('labyrinth-map', defaultMap);
+        window.localStorage.setItem('character-position', defaultCharacterPosition);
+        this.labyrinthMapArray = window.JSON.parse(defaultMap);
+        this.characterPosition = window.JSON.parse(defaultCharacterPosition);
+        this.render();
+      });
+      controlPanelElement.appendChild(defaultBtn);
+
+      if (this.characterSetMode) {
+        const resetCharacterButton = window.document.createElement('button');
+        resetCharacterButton.innerHTML = 'reset character position';
+        resetCharacterButton.addEventListener('click', () => {
+          window.localStorage.removeItem('character-position');
+          this.characterPosition = { h: -1, w: -1 };
+          this.characterSetMode = false;
+          this.render();
+        });
+        controlPanelElement.appendChild(resetCharacterButton);
+      } else {
+        const editCharacterSetModeButton = window.document.createElement('button');
+        editCharacterSetModeButton.innerHTML = 'select character position';
+        editCharacterSetModeButton.addEventListener('click', () => {
+          this.characterSetMode = true;
+          this.render();
+        });
+        controlPanelElement.appendChild(editCharacterSetModeButton);
+      }
+    } else {
+      const mapEditModeButton = window.document.createElement('button');
+      mapEditModeButton.innerHTML = 'edit mode';
+      mapEditModeButton.addEventListener('click', () => {
+        this.mapEditMode = true;
+        this.render();
+      });
+      controlPanelElement.appendChild(mapEditModeButton);
+    }
+    return controlPanelElement
+  }
+
+  getEditorElement() {
+    const editorEl = window.document.createElement('div');
+    editorEl.style.float = 'right';
+    editorEl.style.width = 'calc(100% - 500px)';
+
+    const descriptionEl = window.document.createElement('p');
+    descriptionEl.innerHTML = 'Please implement function <strong>findExit() {}</strong> that will move character to the exit door in given labyrinth. You have method <strong>window.moveCharacter(direction)</strong> that accepts one of four possible directions: "up", "right", "down", "left". Said method moves character and returns 0 if move was failed (end of the map or wall block), 1 if move is completed, and 2 if door is reached. You can edit map with a control above. Edit function below. Press "run code" button to run your code.';
+    editorEl.appendChild(descriptionEl);
+    const runCodeButton = window.document.createElement('button');
+    runCodeButton.innerHTML = 'run code';
+    runCodeButton.addEventListener('click', () => {
+      eval(window.localStorage.getItem('move-character-function'));
+    });
+    editorEl.appendChild(runCodeButton);
+    const preInputEl = window.document.createElement('code');
+    preInputEl.innerHTML = '<hr />function findExit() {<br />';
+    editorEl.appendChild(preInputEl);
+
+    const inputEl = window.document.createElement('code');
+    inputEl.setAttribute('contenteditable', true);
+    inputEl.style.outline = 'none';
+    const moveCharacterFunction = window.localStorage.getItem('move-character-function');
+    inputEl.innerText = moveCharacterFunction;
+    inputEl.addEventListener('keydown', (event) => {
+      event.stopPropagation();
+      window.localStorage.setItem('move-character-function', event.target.innerText);
+    });
+    editorEl.appendChild(inputEl);
+
+    const postInputEl = window.document.createElement('code');
+    postInputEl.innerHTML = '<br/>}';
+    editorEl.appendChild(postInputEl);
+
+    return editorEl;
+  }
+
+  render() {
+    while (window.document.body.firstChild) {
+      window.document.body.removeChild(window.document.body.firstChild);
+    }
+    const rootDiv = window.document.createElement('div');
+    rootDiv.style.margin = '10px';
+    window.document.body.appendChild(rootDiv);
+
+    const controlPannelElement = this.getControlPanelElement();
+    rootDiv.appendChild(controlPannelElement);
+
+    const mapDiv = window.document.createElement('div');
+    mapDiv.style.border = '2px solid saddlebrown';
+    mapDiv.style.display = 'inline-block';
+    rootDiv.appendChild(mapDiv);
+
+    const editorEl = this.getEditorElement();
+    rootDiv.appendChild(editorEl);
+
+    for (let widthRow of this.labyrinthMapArray) {
+      const rowEl = window.document.createElement('div');
+      rowEl.style.height = '32px';
+      for (let cell of widthRow) {
+        const cellElement = this.getCellElement(cell)
+        rowEl.appendChild(cellElement);
+      }
+      mapDiv.appendChild(rowEl);
+    }
+  }
+}
+
+const labyrinth = new Labyrinth();
